c22c03b9d41cfd0e675bca9e23ab141ebbdff476
GreeNM11 <lindanny75@gmail.com>
2025-08-27 10:24:02 -0500

    Addition of Security files

---
+++ game_server/server_security.h
@@ -0,0 +1,13 @@
+#ifndef SERVER_SECURITY_H
+#define SERVER_SECURITY_H
+
+#include <QObject>
+
+class server_security : public QObject
+{
+    Q_OBJECT
+public:
+    server_security();
+};
+
+#endif // SERVER_SECURITY_H
